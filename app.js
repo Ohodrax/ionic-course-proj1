4d176d691b1935fbf496c157c4cad5d5bdc6b935
ohodrax <rodrigo.cassiano00@gmail.com>
2023-05-29 16:25:33 -0300

    Simple challange of courses rating

--- app.js
+++ app.js
@@ -3,24 +3,43 @@ const amountInput = document.querySelector('#input-amount');
 const cancelBtn = document.querySelector('#btn-cancel');
 const confirmBtn = document.querySelector('#btn-confirm');
 const expesesList = document.querySelector('#expenses-list');
+const totalExpensesOutput = document.querySelector('#total-expenses');
+const alertCtrl = document.querySelector('ion-alert-controller');
+
+let totalExpenes = 0;
+
+const clear = () => {
+    reasonInput.value = '';
+    amountInput.value = '';
+}
 
 confirmBtn.addEventListener('click', () => {
     const enteredReason = reasonInput.value;
     const enteredAmount = amountInput.value;
-
+    
     if (
         enteredReason.trim().length <= 0 || 
         enteredAmount <= 0 || 
         enteredAmount.trim().length <= 0
     ) {
+        alertCtrl.create({
+            message: 'Please enter valid reason and amount',
+            header: 'Invalid inputs',
+            buttons: ['Okay']
+        }).then(alertElement => {
+            alertElement.present()
+;        });
         return;
     }
-
+    
     const newItem = document.createElement('ion-item');
     newItem.textContent = enteredReason + ': $' + enteredAmount;
-
+    
     expesesList.appendChild(newItem);
 
-    reasonInput.value = '';
-    amountInput.value = '';
+    totalExpenes += +enteredAmount;
+    totalExpensesOutput.textContent = totalExpenes;
+    clear();
 });
+
+cancelBtn.addEventListener('click', () => clear());
